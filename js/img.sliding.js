e38647317d619b96586cb6759803f840db3ed087
Jay Patel <jay092000patel@gmail.com>
2020-03-13 14:00:37 +0530

    firstcomite

---
+++ js/img.sliding.js
@@ -0,0 +1,21 @@
+$(function() {
+  var body = $("#home");
+  var backgrounds = [
+    "url(../img/IMG_1.JPG) center top",
+    "url(../img/IMG_2.JPG) center top"
+  ];
+  var current = 0;
+  function fade(){body.fadeOut('slow',nextBackground);}
+  function nextBackground() {
+    body.css(
+      "background",
+      backgrounds[(current = ++current % backgrounds.length)]
+    );
+    body.css("backgroundSize", "cover");
+    body.fadeIn(3000);
+    fade();
+  }
+  fade();
+  body.css("background", backgrounds[0]);
+  body.css("backgroundSize", "cover");
+});
