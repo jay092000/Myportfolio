7c0bc997debcd45fc58974b57205487814c2b0d0
Jay Patel <jay092000patel@gmail.com>
2020-03-17 19:26:44 +0530

    init

---
+++ js/download.js
@@ -0,0 +1,37 @@
+
+function myFunction() {
+
+	document.getElementById("button").classList.add("onclic");
+	setTimeout(stopLoding, 4000)
+}
+
+function stopLoding() {
+	console.log("in fuction...");
+	document.getElementById("button").classList.remove("onclic");
+	document.getElementById("button").classList.add("validate");
+
+	setTimeout(continueExecution, 2000)
+}
+
+function continueExecution()
+{
+	document.getElementById("button").classList.remove("validate");
+}
+
+// $( "#button" ).click(function() {
+//   $( "#button" ).addClass( "onclic",validate);
+
+// });
+
+// function validate() {
+//   setTimeout(function() {
+// 	  console.log("-----");
+// 	$( "#button" ).removeClass( "onclic" );
+// 	$( "#button" ).addClass( "validate", 450, callback );
+//   }, 2250 );
+// }
+//   function callback() {
+// 	setTimeout(function() {
+// 	  $( "#button" ).removeClass( "validate" );
+// 	}, 1250 );
+//   }
