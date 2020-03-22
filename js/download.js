22bf5db5d1158d26553f71a50ca0e3377cd0a801
jay092000 <jay092000patel@gmail.com>
2020-03-22 10:53:21 +0530

    init

--- js/download.js
+++ js/download.js
@@ -1,37 +1,15 @@
-
 function myFunction() {
-	
-	document.getElementById("button").classList.add("onclic");
-	setTimeout(stopLoding, 4000)
+  document.getElementById("button").classList.add("onclic");
+  setTimeout(stopLoding, 4000);
 }
 
 function stopLoding() {
-	
-	document.getElementById("button").classList.remove("onclic");
-	document.getElementById("button").classList.add("validate");
+  document.getElementById("button").classList.remove("onclic");
+  document.getElementById("button").classList.add("validate");
 
-	setTimeout(continueExecution, 2000)
+  setTimeout(continueExecution, 2000);
 }
 
-function continueExecution()
-{
-	document.getElementById("button").classList.remove("validate");
+function continueExecution() {
+  document.getElementById("button").classList.remove("validate");
 }
-
-// $( "#button" ).click(function() {
-//   $( "#button" ).addClass( "onclic",validate);
-
-// });
-
-// function validate() {
-//   setTimeout(function() {
-// 	  console.log("-----");
-// 	$( "#button" ).removeClass( "onclic" );
-// 	$( "#button" ).addClass( "validate", 450, callback );
-//   }, 2250 );
-// }
-//   function callback() {
-// 	setTimeout(function() {
-// 	  $( "#button" ).removeClass( "validate" );
-// 	}, 1250 );
-//   }
