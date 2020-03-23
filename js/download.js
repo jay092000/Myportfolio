527c172dc80a3b476229d84e9ef093f57c18a745
jay092000 <jay092000patel@gmail.com>
2020-03-23 12:05:47 +0530

    init

--- js/download.js
+++ js/download.js
@@ -1,15 +1,18 @@
 function myFunction() {
-  document.getElementById("button").classList.add("onclic");
-  setTimeout(stopLoding, 4000);
+
+	document.getElementById("button").classList.add("onclic");
+	setTimeout(stopLoding, 4000)
 }
 
 function stopLoding() {
-  document.getElementById("button").classList.remove("onclic");
-  document.getElementById("button").classList.add("validate");
+	console.log("in fuction...");
+	document.getElementById("button").classList.remove("onclic");
+	document.getElementById("button").classList.add("validate");
 
-  setTimeout(continueExecution, 2000);
+	setTimeout(continueExecution, 2000)
 }
 
-function continueExecution() {
-  document.getElementById("button").classList.remove("validate");
+function continueExecution()
+{
+	document.getElementById("button").classList.remove("validate");
 }
