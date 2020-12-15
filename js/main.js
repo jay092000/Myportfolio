88ec6d98527622c4d5296c75a2a993120bda52e3
jay092000 <jay092000patel@gmail.com>
2020-12-15 18:50:15 +0530

    magic added

--- js/main.js
+++ js/main.js
@@ -1,26 +1,38 @@
-const menuBtn = document.querySelector(".menu-btn");
-const hamburger = document.querySelector(".menu-btn_burger");
-const nav = document.querySelector(".nav");
-const menuNav = document.querySelector(".menu-nav");
-const navItems = document.querySelectorAll(".menu-nav_item");
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn_burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav_item');
 
 let showMenu = false;
 
-menuBtn.addEventListener("click", toggleMenu);
+menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
   if (!showMenu) {
-    hamburger.classList.add("open");
-    nav.classList.add("open");
-    menuNav.classList.add("open");
-    navItems.forEach(item => item.classList.add("open"));
+    hamburger.classList.add('open');
+    nav.classList.add('open');
+    menuNav.classList.add('open');
+    navItems.forEach((item) => item.classList.add('open'));
     showMenu = true;
   } else {
-    hamburger.classList.remove("open");
-    nav.classList.remove("open");
-    menuNav.classList.remove("open");
-    navItems.forEach(item => item.classList.remove("open"));
+    hamburger.classList.remove('open');
+    nav.classList.remove('open');
+    menuNav.classList.remove('open');
+    navItems.forEach((item) => item.classList.remove('open'));
 
     showMenu = false;
   }
 }
+window.addEventListener('loaded', () => {
+  LL.init({
+    lineSize:         0.15,
+    opacityDecrement: 0.55,
+    speedExpFactor:   0.8,
+    lineExpFactor:    0.6,
+    sparklesCount:    65,
+    maxOpacity:       0.99,  // should be a number between [0 ... 1]
+    // texture1:         "http://path_to_texture",      // texture displayed on mouse hover
+    // texture2:         "http://path_to_texture",      // texture displayed on mouse click
+    // texture3:         "http://path_to_texture",      // texture displayed on sparkles
+});});
