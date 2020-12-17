658dda710ff3a9597e9f8ea50763a9a14ef5a271
jay092000 <jay092000patel@gmail.com>
2020-12-17 15:57:44 +0530

    second last push

--- js/work.js
+++ js/work.js
@@ -1,10 +1,15 @@
 function projects(id) {
- console.log(3);
+  for(i=0;i<4;i++)
+  {
+    document.getElementById("projectSelection_"+ (i+1)).classList.remove("activeted_project");
+  }
+  document.getElementById("projectSelection_"+ id).classList.add("activeted_project");
   switch (id) {
     case 1:
       for (i = 0; i < 8; i++) {
         $('#image_' + (i + 1)).fadeIn(800);
       }
+     
       break;
     case 2:
       for (i = 0; i < 3; i++) {
